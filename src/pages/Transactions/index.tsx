4839eaa672e75caeebc6171590976a81229ebb2d
fernandoboscato <fernandoboscato@faee.com.br>
2023-07-21 18:08:13 -0300

    tela

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,35 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionContainer, TransactionTable } from "./styles";
 
 export function Transactions(){
   return(
     <div>
       <Header/>
      <Summary/>
+     <TransactionContainer>
+      <TransactionTable>
+        <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>R$ 12.000,00</td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+        </tbody>
+      </TransactionTable>
+      <TransactionTable>
+        <tbody>
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>- R$ 59,00</td>
+            <td>Alimentação</td>
+            <td>10/04/2022</td>
+          </tr>
+        </tbody>
+      </TransactionTable>
+
+     </TransactionContainer>
     </div>
   )
 }
